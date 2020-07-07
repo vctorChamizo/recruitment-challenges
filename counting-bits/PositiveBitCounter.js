fb56c4280bb6c88fa05a8e5e83f1f13eab4af076
vctorChamizo <victor.chamizo96@gmail.com>
2020-07-07 16:12:16 +0200

    feat: counting bits

--- counting-bits/PositiveBitCounter.js
+++ counting-bits/PositiveBitCounter.js
@@ -1,5 +1,18 @@
-function Count (input) {
-  throw new Error('Not implemented')
+function Count(input) {
+  if (input < 0) throw new RangeError();
+  else if (input === 0) return [0];
+
+  const result = [];
+  const binary = input.toString(2).split("");
+  const size = binary.length;
+
+  for (let i = size - 1; i >= 0; i--) {
+    if (binary[i] > 0) result.push(Math.abs(i - size + 1));
+  }
+
+  result.unshift(result.length);
+
+  return result;
 }
 
-module.exports = { Count }
+module.exports = { Count };
