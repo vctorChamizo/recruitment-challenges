d1a548724c010212ee25a78134825042254532c1
vctorChamizo <victor.chamizo96@gmail.com>
2020-07-08 12:20:21 +0200

    feat: read order class

--- fraud-detection/src/utils/order.js
+++ fraud-detection/src/utils/order.js
@@ -9,6 +9,44 @@ const keyMap = [
   "creditCard",
 ];
 
+/**
+ *
+ * @param {String} email
+ */
+const normalizEmail = (email) => {
+  let aux = email.split("@");
+  let atIndex = aux[0].indexOf("+");
+  aux[0] =
+    atIndex < 0
+      ? aux[0].replace(".", "")
+      : aux[0].replace(".", "").substring(0, atIndex - 1);
+  email = aux.join("@");
+
+  return email;
+};
+
+/**
+ *
+ * @param {String} street
+ */
+const normalizeStreet = (street) =>
+  street.replace(/st.|rd./, (str) => (str === "st." ? "street" : "road"));
+
+const normalizeState = (state) =>
+  state.replace(/il|ca|ny/, (str) =>
+    str === "il"
+      ? "illinois"
+      : str === "ca"
+      ? "california"
+      : str === "cl"
+      ? "colorado"
+      : "new york"
+  );
+
+/**
+ *
+ * @param {Array} order
+ */
 const orderSegregation = (order) => {
   const segregation = {};
 
@@ -17,9 +55,17 @@ const orderSegregation = (order) => {
       index < 2 ? Number(element) : index < 6 ? element.toLowerCase() : element;
   });
 
+  segregation.email = normalizEmail(segregation.email);
+  segregation.street = normalizeStreet(segregation.street);
+  segregation.state = normalizeState(segregation.state);
+
   return segregation;
 };
 
+/**
+ *
+ * @param {Array} orders
+ */
 const normalizeOrders = (orders) =>
   orders.map((element) => orderSegregation(element.split(",")));
 
